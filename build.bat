3aff506ed5ce235dc02ac48ac9605a46680051ab
michalbrabec <gmichal.brabec@gmail.com>
2017-03-29 14:43:54 +0200

    NuGet version

--- build.bat
+++ build.bat
@@ -20,6 +20,7 @@ if not "%errorlevel%"=="0" goto failure
 
 REM Package
 mkdir Build
+call "%nuget%" pack "Source\Devsense.PHP.Parser\Devsense.PHP.Parser.nuspec" -Version=%version%
 if not "%errorlevel%"=="0" goto failure
 
 :success
