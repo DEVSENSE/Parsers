11f7bf3db42d7aa9d72c89ee2cd9876b8d20773f
Jakub Míšek <jmisek@gmail.com>
2017-05-04 18:31:38 +0200

    migrated to msbuild 15

--- build.bat
+++ build.bat
@@ -11,20 +11,15 @@ if not "%PackageVersion%" == "" (
 
 REM Build
 %nuget% restore Source\PhpParser.sln
-"%msbuildexe%" Source\PhpParser.sln /p:Configuration="%config%" /p:SkipAfterBuild="true" /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=Normal /nr:false
+"%msbuildexe%" Source\PhpParser.sln /p:Configuration="%config%",VersionPrefix="%version%" /p:SkipAfterBuild="true" /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=Normal /nr:false
 if not "%errorlevel%"=="0" goto failure
 
 REM Unit tests
 "%programfiles(x86)%\Microsoft Visual Studio\2017\Community\Common7\IDE\MSTest.exe" /testcontainer:Source\UnitTests\bin\Release\UnitTests.dll
 if not "%errorlevel%"=="0" goto failure
 
-REM Package
-mkdir Build
-call "%nuget%" pack "Source\Devsense.PHP.Parser\Devsense.PHP.Parser.nuspec" -o Build -p Configuration=%config% %version%
-if not "%errorlevel%"=="0" goto failure
-
-:success
-exit 0
-
 :failure
 exit -1
+
+:success
+echo "Success."
