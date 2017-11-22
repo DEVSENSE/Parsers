9870f148eef0dbc598563c1ab04984715f809274
Jakub Míšek <jmisek@gmail.com>
2017-11-22 16:17:40 +0100

    PHPDoc tests

---
+++ Source/UnitTests/PHPDocTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+using Devsense.PHP.Text;
+using System.Diagnostics;
+using System.Text;
+using System.Linq;
+using System.Text.RegularExpressions;
+using UnitTests.TestImplementation;
+using Devsense.PHP.Syntax;
+
+namespace UnitTests
+{
+    [TestClass]
+    [DeploymentItem("TestData.csv")]
+    [DeploymentItem("Tokens.php")]
+    public class PHPDocTests
+    {
+        static PHPDocBlock NewPHPDoc(string code)
+        {
+            code = code.Trim();
+            return new PHPDocBlock(code, new Span(0, code.Length));
+        }
+
+        [TestMethod]
+        public void SummaryTest()
+        {
+            var phpdoc = NewPHPDoc(@"
+/**
+ * Summary.
+ */");
+
+            Assert.AreEqual(phpdoc.Summary, "Summary.");
+        }
+    }
+}
