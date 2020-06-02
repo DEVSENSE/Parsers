9d2edf05905397069cd7f62f42826477300fb749
Jakub Míšek <jmisek@gmail.com>
2020-06-02 14:28:10 +0200

    Php80Set

--- Source/Devsense.PHP.Parser/Syntax/LanguageFeatures.cs
+++ Source/Devsense.PHP.Parser/Syntax/LanguageFeatures.cs
@@ -41,42 +41,47 @@ namespace Devsense.PHP.Syntax
         /// <summary>
         /// Allows using short open tags in the script.
         /// </summary>
-        Php54Set = 16,
+        Php54Set = 1 << 4,
 
         /// <summary>
         /// PHP 5.5 feature set.
         /// </summary>
-        Php55Set = Php54Set | 32,
+        Php55Set = Php54Set | (1 << 5),
 
         /// <summary>
         /// PHP 5.6 feature set.
         /// </summary>
-        Php56Set = Php55Set | 64,
+        Php56Set = Php55Set | (1 << 6),
 
         /// <summary>
         /// PHP 7.0 feature set.
         /// </summary>
-        Php70Set = Php56Set | 128,
+        Php70Set = Php56Set | (1 << 7),
 
         /// <summary>
         /// PHP 7.1 feature set.
         /// </summary>
-        Php71Set = Php70Set | 256,
+        Php71Set = Php70Set | (1 << 8),
 
         /// <summary>
         /// PHP 7.2 feature set.
         /// </summary>
-        Php72Set = Php71Set | 512,
+        Php72Set = Php71Set | (1 << 9),
 
         /// <summary>
         /// PHP 7.3 feature set.
         /// </summary>
-        Php73Set = Php72Set | 1024,
+        Php73Set = Php72Set | (1 << 10),
 
         /// <summary>
         /// PHP 7.4 feature set.
         /// </summary>
-        Php74Set = Php73Set | 2048,
+        Php74Set = Php73Set | (1 << 11),
+
+        /// <summary>
+        /// PHP 8.0 feature set.
+        /// </summary>
+        Php80Set = Php74Set | (1 << 12),
     }
 
     public static class LanguageFeaturesExtensions
