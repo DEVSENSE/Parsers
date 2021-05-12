fa5a051f3304178d4b052e918e5287f451490f8e
Jakub Míšek <jmisek@gmail.com>
2021-05-12 10:37:31 +0200

    match test assertion

--- Source/UnitTests/ParserTests.cs
+++ Source/UnitTests/ParserTests.cs
@@ -188,8 +188,11 @@ class X {
 
             foreach (var code in codes)
             {
+                var errors = new TestErrorSink();
                 var unit = new CodeSourceUnit(code, "dummy.php", Encoding.UTF8, features: LanguageFeatures.Php80Set);
-                unit.Parse(new BasicNodesFactory(unit), null);
+                unit.Parse(new BasicNodesFactory(unit), errors);
+
+                Assert.AreEqual(0, errors.Count);
             }
         }
 
