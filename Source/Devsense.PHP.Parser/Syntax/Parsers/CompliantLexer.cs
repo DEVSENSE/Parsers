28ebead2dfb22abc35eb3ff6f34914b1e64b080c
Jakub Míšek <jmisek@gmail.com>
2023-03-18 13:42:23 +0100

    reinterpret "default" as T_NAME_QUALIFIED if after \
    ref https://community.devsense.com/d/878-bug-with-source-code-parsing

--- Source/Devsense.PHP.Parser/Syntax/Parsers/CompliantLexer.cs
+++ Source/Devsense.PHP.Parser/Syntax/Parsers/CompliantLexer.cs
@@ -139,6 +139,7 @@ namespace Devsense.PHP.Syntax
                     // reinterpret T_NAME_QUALIFIED
                     case Tokens.T_GLOBAL:
                     case Tokens.T_STATIC:
+                    case Tokens.T_DEFAULT:
                         if (_backup_token == Tokens.T_NS_SEPARATOR) // after "\", it is treated as identifier. See T_NAME_QUALIFIED in Zend. We don't, since it would break backward compatibility with older parsers.
                         {
                             token = Tokens.T_STRING;
