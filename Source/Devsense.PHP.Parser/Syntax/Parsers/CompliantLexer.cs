33d81c096661331ba1ca92e54b7c54f133d745bb
Jakub Míšek <jmisek@gmail.com>
2023-09-12 15:05:50 +0200

    `\switch` parsed
    ref https://github.com/DEVSENSE/phptools-docs/issues/399

--- Source/Devsense.PHP.Parser/Syntax/Parsers/CompliantLexer.cs
+++ Source/Devsense.PHP.Parser/Syntax/Parsers/CompliantLexer.cs
@@ -149,6 +149,7 @@ namespace Devsense.PHP.Syntax
                     case Tokens.T_INTERFACE:
                     case Tokens.T_TRAIT:
                     case Tokens.T_LIST:
+                    case Tokens.T_SWITCH:
                         if (_backup_token == Tokens.T_NS_SEPARATOR) // after "\", it is treated as identifier. See T_NAME_QUALIFIED in Zend. We don't, since it would break backward compatibility with older parsers.
                         {
                             token = Tokens.T_STRING;
