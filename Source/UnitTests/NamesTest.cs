d3ccc8bb18734cac4126cce65d367029f2441099
Jakub Míšek <jmisek@gmail.com>
2024-05-25 16:27:29 +0200

    test QualifiedName.ToString()

---
+++ Source/UnitTests/NamesTest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+using Devsense.PHP.Text;
+using System.Diagnostics;
+using System.Text;
+using System.Linq;
+using System.Text.RegularExpressions;
+using UnitTests.TestImplementation;
+using Devsense.PHP.Syntax;
+using Devsense.PHP.Ast.DocBlock;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class NamesTest
+    {
+        [TestMethod]
+        public void TestQualifiedNameToString()
+        {
+            var names = new string[]
+            {
+                "",
+                "a",
+                "a\\b\\c",
+            };
+
+            foreach (var fqn in names)
+            {
+                Assert.IsNotNull(fqn);
+                var parsed = QualifiedName.Parse(fqn, false);
+                Assert.AreEqual(fqn, parsed.ToString());
+            }
+        }
+    }
+}
