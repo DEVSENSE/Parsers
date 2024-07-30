f4eb1d45ea07cc64a49286b9691a0e4b27bf91f8
Jakub Míšek <jmisek@gmail.com>
2024-07-30 11:13:06 +0200

    reinterpret \namespace as T_STRING

--- Source/Devsense.PHP.Parser/Syntax/Parsers/CompliantLexer.cs
+++ Source/Devsense.PHP.Parser/Syntax/Parsers/CompliantLexer.cs
@@ -171,6 +171,7 @@ namespace Devsense.PHP.Syntax
                     case Tokens.T_WHILE:
                     case Tokens.T_FOR:
                     case Tokens.T_IF:
+                    case Tokens.T_NAMESPACE:
                         if (_backup_token == Tokens.T_NS_SEPARATOR || // after "\", it is treated as identifier. See T_NAME_QUALIFIED in Zend. We don't, since it would break backward compatibility with older parsers.
                             _backup_token == Tokens.T_NAMESPACE)
                         {
